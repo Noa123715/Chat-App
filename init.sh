41a51beef0ab15d2d6ccecf44f6d6ab2ab72a545
noa123715 <noa123715@gmail.com>
2023-09-26 09:59:24 +0300

    level 10

--- init.sh
+++ init.sh
@@ -6,7 +6,10 @@ if [ $# -nq 0 ]; then
     # Arguments were passed, so use them
     version=$1
 fi
+docker volume create chat-app-data
     docker build -t chat-app:${version}
+docker run -t chat-app-data:/chatApp/data -p 5000:5000 --name chat-App-run
+chat-app:${version}
 
 # # Run the container 
 docker run -p 5000:5000 --name chat-App-run chat-app:${version}
