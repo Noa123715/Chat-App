cfdf5b30ec54f895f76ce0950f8d3b3fa0a7ec16
noa123715 <noa123715@gmail.com>
2023-09-26 15:55:27 +0300

    artifact registery 2.4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,33 +1,82 @@
 #!/bin/bash
 
-version=$1
-commit_hash=$2
-
-# if have time, check validate
-
-# Get the version and commit hash from the user
-if [ -z "$version" ]; then
-  read -p "enter version: " version
+# Check if the script was passed any arguments
+if [ $# -eq 0 ]; then
+    # No arguments were passed, so prompt the user for them
+    # Get the version and commit hash from the user
+    echo "Enter the version number: "
+    read version
+    echo "Enter the commit hash: "
+    read commit_hash
+else
+  # Arguments were passed, so use them
+  version=$1
+  commit_hash=$2
 fi
 
-if [ -z "$commit_hash" ]; then
-  read -p "enter commit hash: " commit_hash
+#!/bin/bash
+
+version="v$version"
+dock_name='Dockerfile'
+
+if [ $# -eq 3 ]; then
+    dock_name=$3
 fi
 
-# Build the image
-docker build -t $version .
+appname="chat-app"
 
-# Tag the app and the image
-git tag v$version $commit_hash
-docker tag $version zipinewhous/chat_app:$version
+if docker image inspect "$appname:$version" &> /dev/null; then
+    read -p "Image $appname:$version already exists. Do you want to rebuild it? (y/n): " rebuild
 
-# Push the image to the repository
-git push --follow-tags origin v$version
-docker push zipinewhous/chat_app:${version}
+    if [[ "$rebuild" == "y" ]]; then
+        # Delete the existing image
+        docker rmi "$appname:$version"
 
-# If something was feild
-if [ "$?" -ne 0 ]; then
-  echo "There was an error deploying the image."
-  exit 1
+        # Build the Docker image
+        docker build -t "${appname}:${version}" . -f "${dock_name}"
+    else
+        echo "Using existing image $appname:$version."
+    fi
+else
+    docker build -t "${appname}:${version}" . -f "${dock_name}"
 fi
 
+# read -p "Do you want to push the image to Artifact Registry? (y/n): " push_to_registry
+
+# if [ "$push_to_registry" = "y" ]; then
+#     # Set the impersonation service account
+#     gcloud config set auth/impersonate_service_account artifact-admin-sa@grunitech-mid-project.iam.gserviceaccount.com
+
+#     # Tag the Docker image for Artifact Registry
+#     docker tag "${appname}:${version}" "me-west1-docker.pkg.dev/grunitech-mid-project/miriam-chat-app-images/chat-app:${version}"
+
+#     # Push the Docker image to Artifact Registry
+#     docker push "me-west1-docker.pkg.dev/grunitech-mid-project/miriam-chat-app-images/chat-app:${version}"
+
+#     if [ $? -eq 0 ]; then
+#         echo "Image successfully pushed to Artifact Registry."
+#     else
+#         echo "Error pushing image to Artifact Registry."
+#         exit 1
+#     fi
+# else
+#     echo "Image not pushed to Artifact Registry."
+# fi
+
+
+read -p "Do you want to push the tag to GitHub? (y/n): " push_tag
+if [[ "$push_tag" == "y" ]]; then
+    # Tag the image with the commit hash
+    git tag ${version} ${commit_hash}
+    git push origin ${version}
+    # Check if the image was pushed successfully
+    if [ $? -ne 0 ]; then
+        echo "Error pushing image to GitHub"
+        exit 1
+    else
+        # Success!
+        echo "Image pushed to GitHub successfully"
+    fi
+else
+    echo "Tag not pushed to GitHub."
+fi
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,20 +2,19 @@
 # ./delete
 # Build the image
 version='latest'
-if [ $# -nq 0 ]; then
+if [ $# -ne 0 ]; then
     # Arguments were passed, so use them
     version=$1
 fi
 docker volume create chat-app-data
-    docker build -t chat-app:${version}
-docker run -t chat-app-data:/chatApp/data -p 5000:5000 --name chat-App-run
-chat-app:${version}
+# docker build -t chat-app:${version} .
+docker run -v chat-app-data:/chatApp/data -p 5000:5000 --name chat-app-run chat-app:${version}
 
-# # Run the container 
-docker run -p 5000:5000 --name chat-App-run chat-app:${version}
+# Run the container 
+#docker run -p 5000:5000 --name chat-app-run chat-app:${version}
 
-# # Run the containet with limit to 2G
-# docker run -it --ulimits nproc=1 --memory=2G chat_app
+# Run the container with limit to 2G
+#docker run -it --ulimits nproc=1 --memory=2G chat_app
 
-# Run the containet with limit to 1G
-# docker run -it --ulimits nproc=1 --memory=1G chat_app
+# Run the container with limit to 1G
+#docker run -it --ulimits nproc=1 --memory=1G chat_app
